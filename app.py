3521fee06b9d8db2df3f7580581a2434c77475c0
Bhumeshwar Katre <132291125+bhumeshwarkatre@users.noreply.github.com>
2025-07-15 11:37:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def generate_certificate_key():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=9))
 
 def generate_qr(data):
-    qr = qrcode.QRCode(box_size=10, border=2)
+    qr = qrcode.QRCode(box_size=10, border=4)
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
